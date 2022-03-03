2e6a3e8532f1b01663200c9ab412ef328a218022
HansaniYasodyaGamage <hansaniyasodya@gmail.com>
2022-03-03 23:11:12 +0530

    commit3

---
+++ BookListRazel/Model/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookListRazel.Model
+{
+    public class ApplicationDBContext : DbContext
+    {
+       public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
